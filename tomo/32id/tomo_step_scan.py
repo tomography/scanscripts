d0325f6aefc7d8d4a8ea4ce01aeb82c72080163d
Barbara Frosik <bfrosik@anl.gov>
2017-03-23 13:43:32 -0500

    Updated parameters in functions controlling verifier process

--- tomo/32id/tomo_step_scan.py
+++ tomo/32id/tomo_step_scan.py
@@ -154,10 +154,10 @@ def full_tomo_scan():
 	print 'start_scan()'
 	init_general_PVs(global_PVs, variableDict)
 	if variableDict.has_key('StopTheScan'):
-		cleanup(global_PVs, variableDict, keys)
+		cleanup(global_PVs, variableDict, HOST, PORT, keys)
 		return
 	#start verifier on remote machine
-        keys.append(start_verifier(INSTRUMENT, None, variableDict))
+        keys.append(start_verifier(INSTRUMENT, None, variableDict, VER_DIR, HOST, PORT))
 	#collect interferometer
 	interf_arrs = []
 	if variableDict.has_key('UseInterferometer') and int(variableDict['UseInterferometer']) > 0:
@@ -204,7 +204,7 @@ def main():
 
 if __name__ == '__main__':
     def on_exit(sig, func=None):
-        cleanup(global_PVs, variableDict, keys)
+        cleanup(global_PVs, variableDict, HOST, PORT, keys)
         sys.exit(0)
     set_exit_handler(on_exit)
 
